d7ce8c367853d01006ed36ca614aa618b2d75023
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-13 13:48:44 +0100

    Seeding the database

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,16 +1,12 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksControllers");
 
-// Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+router.route('/').get(booksController.findAll).post(booksController.create)
 
-// Matches with "/api/books/:id"
 router
-  .route("/:id")
+  .route('/:id')
   .get(booksController.findById)
   .put(booksController.update)
   .delete(booksController.remove);
 
-module.exports = router;
+  module.exports = router;
