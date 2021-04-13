eedc5d0cf5ee70fd9670cbee571ae49a3e79fc72
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-13 14:02:40 +0100

    server file built

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+require('dotenv').config();
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// API routes
+app.use(routes)
+
+// Mongo DB Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`API server now on port:${PORT}`);
+});
