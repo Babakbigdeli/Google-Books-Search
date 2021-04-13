f43b1a307a0228494f2daf913e5e7c5d4a42264c
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-13 12:35:11 +0100

    controller built

---
+++ controllers/booksControllers.js
@@ -0,0 +1,46 @@
+const db = require("../models");
+
+// methods for the bookController
+module.exports = {
+//to find books
+  findAll: function(req, res) {
+    db.Book.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        res.status(422).json(err)
+      });
+  },
+  findById: function(req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        res.status(422).json(err)
+      });
+  },
+  //to add new books to the database
+  create: function(req, res) {
+    db.Book.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        res.status(422).json(err)
+      });
+  },
+  //to update books in the database
+  update: function(req, res) {
+    db.Book.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        res.status(422).json(err)
+      });
+  },
+  //to remove books from the database
+  remove: function(req, res) {
+    db.Book.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        res.status(422).json(err)
+      });
+  }
+};
