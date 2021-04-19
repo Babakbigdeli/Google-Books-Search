6910ebd06d7bc0e4d30ec39fea7ead78331f1811
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-19 16:02:12 +0100

    few parameters of book medel updated

--- models/book.js
+++ models/book.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    _id: { type: String },
     title: { type: String },
-    authors: { type: Array },
+    authors: { type: [String] },
     description: { type: String },
     image: { type: String },
-    link: { type: String }
+    link: { type: String },
+    googleId: { type: String },
 });
 
 const Book = mongoose.model("Book", bookSchema);
