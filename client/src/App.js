708930bb6783ac41ebe2210004c24910d87ce355
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-20 11:46:27 +0100

    unwanted component deleted

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 
 import Search from "./pages/Search";
@@ -16,7 +15,6 @@ class App extends Component {
       <Router>
         <div>
           <Navbar />
-          <Header />
           <Wrapper>
             <Route exact path="/" component={Search} />
             <Route exact path="/search" component={Search} />
