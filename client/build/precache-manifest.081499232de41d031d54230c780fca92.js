7870dccd7c2bb59f2dbc7f49ac5e23095a194a85
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-20 14:48:18 +0100

    Production build done

---
+++ client/build/precache-manifest.081499232de41d031d54230c780fca92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54ba7a9e5cf8cb8ed6714d49317b378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db00c9e606aa2b154beb",
+    "url": "/static/css/main.fffd4f7f.chunk.css"
+  },
+  {
+    "revision": "7f6757f44fef0e942dc0",
+    "url": "/static/js/2.0b2337f1.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/static/js/2.0b2337f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db00c9e606aa2b154beb",
+    "url": "/static/js/main.1779b2bf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
