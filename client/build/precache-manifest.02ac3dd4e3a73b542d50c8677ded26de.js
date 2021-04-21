9c9360b91f3dfdd0af4f8a39f1998f1ebef5ee67
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-21 10:58:19 +0100

    build

---
+++ client/build/precache-manifest.02ac3dd4e3a73b542d50c8677ded26de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc671b8e98a2b28686efa70d62174262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aac485201b79a9d2728",
+    "url": "/static/css/main.b9e014e5.chunk.css"
+  },
+  {
+    "revision": "7f6757f44fef0e942dc0",
+    "url": "/static/js/2.0b2337f1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0b2337f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aac485201b79a9d2728",
+    "url": "/static/js/main.f37b8b41.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a12edba8cda24726fa64e9b154c2f2d4",
+    "url": "/static/media/LibraryBackGround.a12edba8.jpg"
+  }
+]);
